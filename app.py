1905463a2a64f57fe92d78d83da32daa7ee5f66b
Gautam <gautam.kumar@continental-corporation.com>
2017-02-28 17:56:36 +0530

    Added init and view file

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return render_template('index.html')
-	
+@app.route("/PythonApp/login", methods=[POST, GET])
+def login():
+    if request.method == POST:
+        user = request.form('nm')
+        return redirect(url_for('sucsess', name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('sucsess', name = user))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
